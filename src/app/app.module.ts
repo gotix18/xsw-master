10fc0f5ddb3ce22e36d0aefc835d300ef0dc361e
Igor <igorkerin91@gmail.com>
2020-06-11 17:15:15 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
